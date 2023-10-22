369f4720e28fcb2a752c63efcf6e779d278faa4b
ImPabl0 <leftgame7@gmail.com>
2023-10-22 19:25:35 -0300

    Design reformulado

--- lib/Views/Widgets/card_pedido.dart
+++ lib/Views/Widgets/card_pedido.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:lembrete_cakes/Responsive/responsive.dart';
 
 import '../../Models/pedido.dart';
 
@@ -13,16 +12,13 @@ class CardPedido extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Responsive responsive = Responsive(context: context);
-    return Flexible(
-        child: Container(
-      margin: EdgeInsets.only(bottom: responsive.altura * 0.015),
-      padding: EdgeInsets.symmetric(horizontal: responsive.largura * 0.03),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
       width: double.infinity,
       height: 75,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(7),
-          border: Border.all(color: const Color(0xFFE5E2E1))),
+          border: Border.all(width: 2, color: const Color(0xFFE5E2E1))),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
@@ -34,32 +30,30 @@ class CardPedido extends StatelessWidget {
                   border: Border.all(color: const Color(0xFFE4A8AB)))),
           Flexible(
               child: Container(
-            margin: EdgeInsets.only(left: responsive.largura * 0.03),
+            margin: const EdgeInsets.only(left: 13),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   pedido.cliente.nome,
-                  style: TextStyle(
-                      fontSize: responsive.fractional * 1.5,
-                      color: const Color(0xFF493A33)),
+                  style:
+                      const TextStyle(fontSize: 15, color: Color(0xFF493A33)),
                 ),
                 Row(
                   children: [
                     Transform.scale(
                       scale: 1.3,
-                      child: Icon(
+                      child: const Icon(
                         Icons.schedule,
-                        size: responsive.fractional * 1.5,
-                        color: const Color(0xFF493A33),
+                        size: 15,
+                        color: Color(0xFF493A33),
                       ),
                     ),
                     Expanded(
                       child: Container(
                           alignment: Alignment.centerLeft,
-                          margin:
-                              EdgeInsets.only(left: responsive.largura * 0.01),
+                          margin: const EdgeInsets.only(left: 10),
                           child: Text(pedido.dataDaEntrega.horaFormatada)),
                     ),
                   ],
@@ -67,12 +61,12 @@ class CardPedido extends StatelessWidget {
               ],
             ),
           )),
-          Icon(
+          const Icon(
             Icons.info_outline,
-            color: const Color(0xFFE4A8AB),
+            color: Color(0xFFE4A8AB),
           )
         ],
       ),
-    ));
+    );
   }
 }
