9120e3a2f16db7e8cc3c2c03f1daea3db8dc16cf
ImPabl0 <leftgame7@gmail.com>
2023-10-22 19:24:49 -0300

    removido presenter

--- lib/Presenters/index_presenter.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import '../Models/pedido.dart';
-
-class IndexPresenter {
-  List<Pedido> pedidos;
-  IndexPresenter({
-    this.pedidos = const [],
-  });
-}
