b14beaed13ba3e453d6d8b65c58dff6524f41adc
ImPabl0 <leftgame7@gmail.com>
2023-11-15 14:54:49 -0300

    Implementado novos eventos e estados para buscar os pedidos

--- lib/Features/common/pedidos_controller/bloc/pedidos_event.dart
+++ lib/Features/common/pedidos_controller/bloc/pedidos_event.dart
@@ -1,6 +1,5 @@
 part of 'pedidos_bloc.dart';
 
-@immutable
 abstract class PedidosEvent {}
 
 class BuscarTodosOsPedidosEvent extends PedidosEvent {}
--- lib/Features/common/pedidos_controller/bloc/pedidos_state.dart
+++ lib/Features/common/pedidos_controller/bloc/pedidos_state.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, must_be_immutable
 part of 'pedidos_bloc.dart';
 
 @immutable
@@ -9,6 +9,14 @@ abstract class PedidosState {
   });
 }
 
-class PedidosInitial extends PedidosState {
-  PedidosInitial({required super.pedidos});
+class CarregandoPedidosState extends PedidosState {
+  CarregandoPedidosState({required super.pedidos});
+}
+
+class PedidosCarregadosState extends PedidosState {
+  PedidosCarregadosState({required super.pedidos});
+}
+
+class PedidosInitialState extends PedidosState {
+  PedidosInitialState({required super.pedidos});
 }
