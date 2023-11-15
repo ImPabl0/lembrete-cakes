3d7c164e33542b7dd815b8d7e460c536071455d0
ImPabl0 <leftgame7@gmail.com>
2023-11-15 14:58:36 -0300

    adicionada ID à forma de pagamento

--- lib/Models/forma_de_pagamento.dart
+++ lib/Models/forma_de_pagamento.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class FormaDePagamento {
   int id;
@@ -6,4 +8,23 @@ class FormaDePagamento {
     required this.id,
     required this.nome,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'nome': nome,
+    };
+  }
+
+  factory FormaDePagamento.fromMap(Map<String, dynamic> map) {
+    return FormaDePagamento(
+      id: map['id'] as int,
+      nome: map['nome'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory FormaDePagamento.fromJson(String source) =>
+      FormaDePagamento.fromMap(json.decode(source) as Map<String, dynamic>);
 }
